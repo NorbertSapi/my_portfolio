12a6789ab4d4d53e4c1adb4be37cd22d3efe372f
Norbert Sapi <ncs.sapi@gmail.com>
2023-03-13 01:17:29 +0000

    design principles and about sections

--- resources/index.js
+++ resources/index.js
@@ -15,3 +15,43 @@ navbarToggle.addEventListener("click", toggleNavbarVisibility);
 
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
+
+
+// Enlarge my chess cake image
+
+function originalImg() {
+  $("#chess").attr('src', './resources/gallery/originalIMG.jpg');
+}
+
+function cakeImg() {
+  $("#chess").attr('src', './resources/gallery/chessboard.jpg');
+}
+
+// Daily Fortune 
+
+let fortunes = ['A beautiful, smart, and loving person will be coming into your life.',
+  'A fresh start will put you on your way.',
+  'A golden egg of opportunity falls into your lap this month.',
+  'A smile is your personal welcome mat.',
+  'All your hard work will soon pay off.'
+]
+
+let button = document.getElementById('fortuneButton');
+let fortune = document.getElementById('fortune');
+
+function fortuneSelector(){
+  let randomFortune = Math.floor(Math.random() * fortunes.length);
+  return fortunes[randomFortune];
+}
+
+function showFortune(){
+  fortune.innerHTML = fortuneSelector();
+  button.innerHTML = 'Come back tomorrow!';
+  button.style.cursor = 'default';
+
+
+  button.removeEventListener('click', showFortune);
+}
+
+button.addEventListener('click', showFortune);
